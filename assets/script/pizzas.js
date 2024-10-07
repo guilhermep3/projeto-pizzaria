e21474049423f830d16fdaffc9ab0ed52c0b05ee
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2024-10-07 14:08:37 -0300

    first updates in script.js

--- assets/script/pizzas.js
+++ assets/script/pizzas.js
@@ -1,14 +1,19 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'images/pizza1.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Portuguesa', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:8, name:'Legumes', img:'images/pizza8.png', price:18.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:9, name:'Tomate', img:'images/pizza9.png', price:19.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:10, name:'Frango com Catupiry', img:'images/pizza10.png', price:21.99, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:11, name:'Napolitana', img:'images/pizza11.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:12, name:'Morango e Nutella', img:'images/pizza12.png', price:24.50, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+    {id:1, name:'Mussarela', img:'assets/images/pizza1.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'assets/images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'assets/images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'assets/images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Portuguesa', img:'assets/images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'assets/images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'assets/images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:8, name:'Legumes', img:'assets/images/pizza8.png', price:18.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:9, name:'Tomate', img:'assets/images/pizza9.png', price:19.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:10, name:'Frango com Catupiry', img:'assets/images/pizza10.png', price:21.99, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:11, name:'Napolitana', img:'assets/images/pizza11.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:12, name:'Morango e Nutella', img:'assets/images/pizza12.png', price:24.50, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
 ];
+let promotionJson = [
+    {id:1, name:'Frango com Catupiry', img:'assets/images/pizza10.png', price:19.00, sizes:['320g','530g','860g'], description:'Promoção com desconto pizza em oferta'},
+    {id:2, name:'Coca-Cola', img:'assets/images/cocacola.png', price:7.00, sizes:['2L','3L'], description:'O melhor preço para você'},
+    {id:3, name:'Guaraná', img:'assets/images/guarana.png', price:5.50, sizes:['2L','3L'], description:'Super oferta somente hoje'}
+]
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,16 @@
+let qs = (el)=>{
+   return document.querySelector(el);
+};
+let qsa = (el)=>{
+   return document.querySelectorAll(el);
+};
+
+promotionJson.map((item, index)=>{
+   let promoOption = document.querySelector('.promoOption').cloneNode(true);
+   document.querySelector('.promotionsOptions').append(promoOption);
+});
+
 pizzaJson.map((item, index)=>{
-   console.log(item, index)
-})
+   let pizzaItem = document.querySelector('.menuOption').cloneNode(true);
+   document.querySelector('.menuOptions').append(pizzaItem);
+});
